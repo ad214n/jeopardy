ddb11fa7e9240599535129f5325aa617aa5b51d3
Adrian Altares <adrianaltares@Adrians-MacBook-Pro.local>
2018-10-12 17:42:17 -0400

    make modal disappear after clicking on answer

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 let points = document.querySelector("h2");
 
 let scores = 0;
-let a = document.querySelector("#one");
-let b = document.querySelector("#two");
-let c = document.querySelector("#three");
+let a = document.querySelector("#answer");
+let b = document.querySelector("#no");
+let c = document.querySelector("#no2");
 
-a.addEventListener('click', function(){
-    if (a == "drake")
-     scores == 100
-    scores.innerHTML = scores    
-    });
+
+   
+    a.addEventListener('click', function(){
+        scores += 100
+        points.innerHTML = scores
+                     
+        })
+    b.addEventListener('click', function(){
+        scores -= 100
+        points.innerHTML = scores
+    })
