ce3a2654125d1bd70010d16077485324aa0410f3
Adrian Altares <adrianaltares@Adrians-MacBook-Pro.local>
2018-10-12 17:12:20 -0400

    added img and scoreboard

---
+++ main.js
@@ -0,0 +1,12 @@
+let points = document.querySelector("h2");
+
+let scores = 0;
+let a = document.querySelector("#one");
+let b = document.querySelector("#two");
+let c = document.querySelector("#three");
+
+a.addEventListener('click', function(){
+    if (a == "drake")
+     scores == 100
+    scores.innerHTML = scores    
+    });
